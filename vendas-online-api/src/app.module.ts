6ab4a0873790a7064d6b50cdede5841181a0b168
Bruno Eduardo de Moura Ferreira <bruno.eduardo.ferreira.dev@gmail.com>
2023-05-24 10:32:50 -0300

    Configuraçao do typeorm no module

--- vendas-online-api/src/app.module.ts
+++ vendas-online-api/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
     }),
     UserModule,
   ],
