7f0315dfec14724b85c6ca8dab109be3d49ef3dc
Bruno Eduardo de Moura Ferreira <bruno.eduardo.ferreira.dev@gmail.com>
2023-05-24 11:46:12 -0300

    atualizando o nome do arquivo da entidade

--- vendas-online-api/src/user/user.service.ts
+++ vendas-online-api/src/user/user.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
-import { UserEntity } from './interfaces/user.interface';
+import { UserEntity } from './interfaces/user.entity';
 import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
-
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const saltOrRounds = 10;
     const passwordHashed = await hash(createUserDto.password, saltOrRounds);
