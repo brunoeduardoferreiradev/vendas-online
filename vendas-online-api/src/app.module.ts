8071f03431cb7fbe97c0ce2c7faad804dacc2ba0
Bruno Eduardo de Moura Ferreira <bruno.eduardo.ferreira.dev@gmail.com>
2023-05-24 10:36:42 -0300

    adicionando o tipo do banco onde sera feita a conexão

--- vendas-online-api/src/app.module.ts
+++ vendas-online-api/src/app.module.ts
@@ -14,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.DB_PASSWORD,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
+      type: 'postgres',
     }),
     UserModule,
   ],
