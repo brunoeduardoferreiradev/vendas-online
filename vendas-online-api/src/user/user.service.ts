eb71049b4a59b42bad607c269776d4e02bf6b926
Bruno Eduardo de Moura Ferreira <bruno.eduardo.ferreira.dev@gmail.com>
2023-05-24 11:58:44 -0300

    trocando a logica de armazenamento local para o banco de dados a tablea user

--- vendas-online-api/src/user/user.service.ts
+++ vendas-online-api/src/user/user.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './interfaces/user.entity';
 import { hash } from 'bcrypt';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const saltOrRounds = 10;
+
     const passwordHashed = await hash(createUserDto.password, saltOrRounds);
 
-    const user: UserEntity = {
+    return this.userRepository.save({
       ...createUserDto,
-      id: this.users.length + 1,
       password: passwordHashed,
-    };
-
-    this.users.push(user);
-
-    return user;
+    });
   }
 
   async getAllUser(): Promise<UserEntity[]> {
-    return this.users;
+    return this.userRepository.find();
   }
 }
