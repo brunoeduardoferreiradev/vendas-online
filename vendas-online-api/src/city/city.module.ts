8ec2bc2a7bdd0585d061f2d38203127af30ab522
Bruno Eduardo de Moura Ferreira <bruno.eduardo.ferreira.dev@gmail.com>
2023-05-24 23:10:25 -0300

    Fazendo cache de cities

--- vendas-online-api/src/city/city.module.ts
+++ vendas-online-api/src/city/city.module.ts
@@ -3,9 +3,15 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { CityEntity } from './entities/city.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CityEntity])],
+  imports: [
+    CacheModule.register({
+      ttl: 9000000000, // Tempo em que os dados ficaram em cache esta em milisegundos
+    }),
+    TypeOrmModule.forFeature([CityEntity]),
+  ],
   controllers: [CityController],
   providers: [CityService],
 })
--- vendas-online-api/src/city/city.service.ts
+++ vendas-online-api/src/city/city.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -8,13 +11,31 @@ export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
-    return this.cityRepository.find({
+    // Verifica se a pesquisa que esta sendo feita no momento esta em cache
+    const citiesCache: CityEntity[] = await this.cacheManager.get(
+      `state_${stateId}`,
+    );
+
+    // Se encontrou a pesquisa ele retorna ela do cache
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    // Se não encontrou faz a pesquisa no banco de dados
+    const cities = await this.cityRepository.find({
       where: {
         stateId,
       },
     });
+
+    // Armazena no chache
+    await this.cacheManager.set(`state_${stateId}`, cities);
+
+    // retorna para visualização
+    return cities;
   }
 }
